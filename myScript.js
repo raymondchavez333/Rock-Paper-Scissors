f2c979b48ec1f7896d9411245b6ce8304812d7b2
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2024-11-23 07:51:02 +0800

    Add an if else if condition in getComputerChoice function

--- myScript.js
+++ myScript.js
@@ -1,3 +1,10 @@
 function getComputerChoice(){
-    
+    const randNumber = Math.floor(Math.random()*3);
+    if(randNumber == 0){
+        return 'rock';        
+    } else if(randNumber == 1) {
+        return 'paper';
+    }
+
+    return 'scissors';
 }
