2cdb85af294ae6668ede78ad11d39ebe6ce8e450
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2024-11-22 15:30:50 +0800

    Add a switch statement within the getComputerChoice

--- myScript.js
+++ myScript.js
@@ -1,4 +1,12 @@
 function getComputerChoice(){
     let randNum = Math.floor(Math.random()*3);
     
+    switch (randomNumber){
+        case 0:
+            return 'ROCK';
+        case 1:
+            return 'PAPER';
+        case 2:
+            return 'SCISSORS';
+    }
 }
