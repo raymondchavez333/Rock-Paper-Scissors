2c71c352677bb2eacc00518e3e874e95a1c32fb3
raymondchavez333 <94435919+raymondchavez333@users.noreply.github.com>
2024-12-17 07:26:32 +0800

    modify the step 2

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function getComputerChoice(){
-    return Math.floor(Math.random()*3);
-}
-let computerChoice= '';
-if (getComputerChoice()=== 0){
-    computerChoice= "rock";
-}else if (getComputerChoice()===1){
-    computerChoice= "paper";
-}else{
-    computerChoice= "scissors";
+    let random= Math.floor(Math.random()*3);
+
+    let computerChoice= '';
+    if (getComputerChoice()=== 0){
+        computerChoice= "rock";
+    }else if (getComputerChoice()===1){
+        computerChoice= "paper";
+    }else{
+        computerChoice= "scissors";
+    }
+
 }
 
-console.log(computerChoice);
+
+function getHumanChoice(){
+    let askHuman= prompt("rock, paper or scissors?");
+
+    let HumanChoice='';
+    if (askHuman.toLowerCase()=== "rock"){
+        HumanChoice= "rock";
+    }else if(askHuman.toLowerCase()=== "paper"){
+        HumanChoice= "paper";
+    }else {
+        HumanChoice= "scissors";
+    }
+}
+
+
+
